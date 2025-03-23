3675529c1a32f2a207b6bcd669dd5b54bf741c9f
Winicius Silva <winiciusab12@gmail.com>
2025-03-23 12:24:04 -0300

    add: arena type field

--- app/api/models/arena.py
+++ app/api/models/arena.py
@@ -1,10 +1,20 @@
+from enum import Enum
+
 from sqlmodel import Field, SQLModel
+
+
+class ArenaType(str, Enum):
+    VOLEI = "VOLEI"
+    BEACH_TENNIS = "BEACH_TENNIS"
+    SOCIETY = "SOCIETY"
+    TENIS = "TÊNIS"
 
 
 class ArenaBase(SQLModel):
     name: str
     description: str
     capacity: int
+    type: ArenaType
 
 
 class Arena(ArenaBase, table=True):
