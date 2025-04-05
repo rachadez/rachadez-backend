5f76a638c7a8b0673874f0b74d52eb56f3a13ab3
Jose-Jardel-Alves <jose.jardel.alves70@gmial.com>
2025-04-05 11:54:02 -0300

    tests(user and login): add tests for signup, create_user_internal and login

--- app/tests/api/routes/test_user.py
+++ app/tests/api/routes/test_user.py
@@ -42,7 +42,35 @@ class TestUserRoutes:
             headers={"Content-Type": "application/x-www-form-urlencoded"},
         )
 
-        assert response.status_code == 200, response.text
+        assert response.status_code == 200
+    
+
+    def test_login_access_incorrect_email(self, client, setUp):
+        response = client.post(
+            "/v1/login/access-token",
+            data={
+                "username": "emailerrado@example.ufcg.edu.br",
+                "password": "admin password",
+            },
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+        assert response.status_code == 400, response.text
+        assert response.json()["detail"] == "Email ou senha incorretos."
+    
+    def test_login_access_incorrect_password(self, client, setUp):
+        response = client.post(
+            "/v1/login/access-token",
+            data={
+                "username": "admin@example.ufcg.edu.br",
+                "password": "senha incorreta",
+            },
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+        assert response.status_code == 400, response.text
+        assert response.json()["detail"] == "Email ou senha incorretos."
+
 
     def test_get_users(self, client, setUp):
         access_token_response = client.post(
@@ -158,3 +186,224 @@ class TestUserRoutes:
         )
 
         assert created.json()["email"] == data["email"]
+    
+    def test_create_internal_user_email_already_exist(self, client, setUp):
+        data = {
+            "email": "user@example.ufcg.edu.br",
+            "cpf": "12345678911",
+            "phone": "83999124702",
+            "occupation": "ALUNO",
+            "is_active": True,
+            "is_admin": False,
+            "is_internal": True,
+            "full_name": "Internal User",
+            "password": "internal user password",
+        }
+
+        access_token_response = client.post(
+            "/v1/login/access-token",
+            data={
+                "username": "admin@example.ufcg.edu.br",
+                "password": "admin password",
+            },
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+        token = access_token_response.json()["access_token"]
+
+        response = client.post(
+            USER_PREFIX + "/",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            json=data,
+        )
+
+        assert response.status_code == 200
+
+        data_second_user = {
+            "email": "user@example.ufcg.edu.br",
+            "cpf": "12345678922",
+            "phone": "83999124702",
+            "occupation": "ALUNO",
+            "is_active": True,
+            "is_admin": False,
+            "is_internal": True,
+            "full_name": "Internal User",
+            "password": "internal user password",
+        }
+
+        response_second_user = client.post(
+            USER_PREFIX + "/",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            json=data_second_user,
+        )
+
+        assert response_second_user.status_code == 400
+        assert response_second_user.json()["detail"] == "Já existe um usuário com esse e-mail."
+
+    def test_create_internal_user_cpf_already_exist(self, client, setUp):
+        data = {
+            "email": "user@example.ufcg.edu.br",
+            "cpf": "12345678911",
+            "phone": "83999124702",
+            "occupation": "ALUNO",
+            "is_active": True,
+            "is_admin": False,
+            "is_internal": True,
+            "full_name": "Internal User",
+            "password": "internal user password",
+        }
+
+        access_token_response = client.post(
+            "/v1/login/access-token",
+            data={
+                "username": "admin@example.ufcg.edu.br",
+                "password": "admin password",
+            },
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
+
+        token = access_token_response.json()["access_token"]
+
+        response = client.post(
+            USER_PREFIX + "/",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            json=data,
+        )
+
+        assert response.status_code == 200
+
+        data_second_user = {
+            "email": "user2@example.ufcg.edu.br",
+            "cpf": "12345678911",
+            "phone": "83999124702",
+            "occupation": "ALUNO",
+            "is_active": True,
+            "is_admin": False,
+            "is_internal": True,
+            "full_name": "Internal User",
+            "password": "internal user password",
+        }
+
+        response_second_user = client.post(
+            USER_PREFIX + "/",
+            headers={
+                "Authorization": f"Bearer {token}",
+                "accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            json=data_second_user,
+        )
+
+        assert response_second_user.status_code == 400
+        assert response_second_user.json()["detail"] == "Já existe um usuário com esse CPF."
+
+
+
+
+    def test_signup(self, client, setUp):
+
+        data = {
+            "email": "user@ccc.ufcg.edu.br",
+            "password": "senha1234",
+            "cpf": "12345678911",
+            "phone": "83911223344",
+            "occupation": "ALUNO",
+            "full_name": "user"
+        }
+
+        response = client.post(
+            USER_PREFIX + "/signup",
+            json=data
+        )
+
+        assert response.status_code == 200
+        assert data["email"] == response.json()["email"]
+        assert "id" in response.json()
+    
+    def test_singup_user_email_already_exist(self, client, setUp):
+
+        data = {
+            "email": "user@ccc.ufcg.edu.br",
+            "password": "senha1234",
+            "cpf": "11111111111",
+            "phone": "83911111111",
+            "occupation": "ALUNO",
+            "full_name": "user"
+        }
+
+        response = client.post(
+            USER_PREFIX + "/signup",
+            json=data
+        )
+
+        assert response.status_code == 200
+
+        data2 = {
+            "email": "user@ccc.ufcg.edu.br",
+            "password": "senha1234",
+            "cpf": "22222222222",
+            "phone": "83922222222",
+            "occupation": "ALUNO",
+            "full_name": "user"
+        }
+
+        response2 = client.post(
+            USER_PREFIX + "/signup",
+            json=data2
+        )
+
+        assert response2.status_code == 400
+        assert response2.json()["detail"] == "Já existe um usuário com esse e-mail."
+    
+    def test_singup_user_cpf_already_exist(self, client, setUp):
+
+        data = {
+            "email": "user@ccc.ufcg.edu.br",
+            "password": "senha1234",
+            "cpf": "11111111111",
+            "phone": "83911111111",
+            "occupation": "ALUNO",
+            "full_name": "user"
+        }
+
+        response = client.post(
+            USER_PREFIX + "/signup",
+            json=data
+        )
+
+        assert response.status_code == 200
+
+        data2 = {
+            "email": "user2@ccc.ufcg.edu.br",
+            "password": "senha1234",
+            "cpf": "11111111111",
+            "phone": "83922222222",
+            "occupation": "ALUNO",
+            "full_name": "user"
+        }
+
+        response2 = client.post(
+            USER_PREFIX + "/signup",
+            json=data2
+        )
+
+        assert response2.status_code == 400
+        assert response2.json()["detail"] == "Já existe um usuário com esse CPF."
+
+
+
+
+
+
